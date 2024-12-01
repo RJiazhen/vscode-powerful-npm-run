60d5ae0e06ff702579521ec2c4e92a88a614b95d
RJiazhen <jiazhen.ruan@outlook.com>
2024-12-01 11:10:00 +0800

    refactor: optimize npm script execution and terminal commands

--- src/features/npm-run/open-script-in-terminal.ts
+++ src/features/npm-run/open-script-in-terminal.ts
@@ -1,8 +1,8 @@
-import { Terminal, window, workspace } from "vscode";
-import { checkIsNvmrcExit } from "../../utils/check-is-nvmrc-exit";
-import { getVersionInNvmrc } from "../../utils/get-version-in-nvmrc";
-import { getPowerfulNpmRunConfiguration } from "../../utils/get-powerful-npm-run-configuration";
+import { Terminal } from "vscode";
 import { ConfigurationSection } from "../../constants/enums/configuration";
+import { checkIsNvmrcExit } from "../../utils/check-is-nvmrc-exit";
+import { getPowerfulNpmRunConfiguration } from "../../utils/get-powerful-npm-run-configuration";
+import { getVersionInNvmrc } from "../../utils/get-version-in-nvmrc";
 
 /**
  * Opens a terminal and runs the selected npm script in it.
@@ -13,25 +13,23 @@ export const openScriptInTerminal = async (
   terminal: Terminal,
   selectedNpmScript: NpmScriptQuickPickItem,
 ) => {
-  terminal.show();
   const workPath = selectedNpmScript.packageJsonPath.replace(
     "package.json",
     "",
   );
-  terminal.sendText(`cd ${workPath}`);
 
-  const isNvmrcExit = checkIsNvmrcExit(
-    selectedNpmScript.packageJsonPath.replace("package.json", ""),
-  );
+  const isNvmrcExit = checkIsNvmrcExit(workPath);
 
-  const nvmrcVersion = isNvmrcExit
-    ? await getVersionInNvmrc(
-        selectedNpmScript.packageJsonPath.replace("package.json", ".nvmrc"),
-      )
+  const nodeVersion = isNvmrcExit
+    ? await getVersionInNvmrc(workPath + ".nvmrc")
     : getPowerfulNpmRunConfiguration(ConfigurationSection.defaultNodeVersion);
 
-  terminal.sendText(`nvm use ${nvmrcVersion}`);
+  const terminalTextList = [
+    `cd ${workPath}`,
+    nodeVersion ? `nvm use ${nodeVersion}` : "",
+    `npm run ${selectedNpmScript.name}`,
+  ];
 
-  const npmCommand = `npm run ${selectedNpmScript.name}`;
-  terminal.sendText(npmCommand);
+  terminal.show();
+  terminal.sendText(terminalTextList.join("\n"));
 };
